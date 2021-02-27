0deade75665a756680abdaad85a7c379ba2ade88
Christian Henry <christian.henry92@gmail.com>
2021-02-26 19:23:07 -0500

    Delete default UI components

--- lib/apertivo_web/live/page_live.ex
+++ lib/apertivo_web/live/page_live.ex
@@ -7,7 +7,7 @@ defmodule ApertivoWeb.PageLive do
   def mount(_params, _session, socket) do
     map_api_key = Application.get_env(:apertivo, ApertivoWeb.Endpoint)[:google_maps_api_key]
 
-    {:ok, assign(socket, query: "", results: %{}, map_api_key: map_api_key)}
+    {:ok, assign(socket, map_api_key: map_api_key)}
   end
 
   @impl true
@@ -27,36 +27,5 @@ defmodule ApertivoWeb.PageLive do
 
   defp rand_float(min, max) do
     :random.uniform() * (max - min) + min
-  end
-
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not ApertivoWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
   end
 end
