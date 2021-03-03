5f1c197c65820e38d807a2a4c96649a3b1fd78a6
Christian Henry <christian.henry92@gmail.com>
2021-03-02 21:47:09 -0500

    Define seeds to seed happy hour table

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,26 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+defmodule X do
+  alias Apertivo.HappyHours.HappyHour
+
+  def convert(json) do
+    %HappyHour{
+      city: json["city"],
+      location: %Geo.Point{
+        coordinates: {json["latLng"]["longitude"], json["latLng"]["latitude"]},
+        srid: 4326
+      },
+      link: json["link"],
+      place_id: json["placeId"],
+      restaurant: json["restaurant"],
+      schedule: json["schedule"]
+    }
+  end
+end
+
+results =
+  File.read!("results.json")
+  |> Jason.decode!()
+  |> Enum.map(fn hh -> X.convert(hh) end)
+  |> Enum.each(fn hh -> Apertivo.Repo.insert!(hh) end)
