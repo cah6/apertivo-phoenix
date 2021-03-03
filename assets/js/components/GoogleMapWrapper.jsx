3e5cb91e754717ca73edbd7ae5bb747951f9934c
Christian Henry <christian.henry92@gmail.com>
2021-03-03 10:19:00 -0500

    Scroll such it brings element to middle of reel

--- assets/js/components/GoogleMapWrapper.jsx
+++ assets/js/components/GoogleMapWrapper.jsx
@@ -81,7 +81,7 @@ function GoogleMapWrapper(props) {
 
         document
           .getElementById(`reel-item-${selectedId}`)
-          .scrollIntoView({ behavior: "smooth" });
+          .scrollIntoView({ behavior: "smooth", inline: "center" });
       });
     }
   });
